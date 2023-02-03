86378ebd65a69dd32d5534e17c3edabbdd83ac4f
MichalS <99993669+MichalSiki@users.noreply.github.com>
2023-02-03 11:51:10 +0100

    g

--- IT_Network_JAVA_OOP_zkouseni/src/HelloObjects.java
+++
@@ -1,9 +0,0 @@
-public class HelloObjects {
-    public static void main(String[] args) {
-       Zdravic zdravic = new Zdravic();
-       zdravic.text = "By dem";
-        System.out.println(zdravic.pozdrav("Michal"));
-       zdravic.pozdrav("Pepa");
-
-    }
-}
